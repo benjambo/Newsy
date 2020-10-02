55a6168aeed70c8824755146a1fea6183d3c54c0
Benjamin Bowo <ben.bowo@gmail.com>
2020-10-03 01:18:18 +0300

    Updates

---
+++ precache-manifest.da7bcb40bb920637b48119c828ba1ab3.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7647dce6a18bb9c8bab5c8978d0786",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "4dcf17aaa84cbcf60bf8",
+    "url": "/NewsApp/static/css/2.cbbea2e1.chunk.css"
+  },
+  {
+    "revision": "d6c0b92a3de531aaa3a8",
+    "url": "/NewsApp/static/css/main.4d9f29ea.chunk.css"
+  },
+  {
+    "revision": "4dcf17aaa84cbcf60bf8",
+    "url": "/NewsApp/static/js/2.4847d574.chunk.js"
+  },
+  {
+    "revision": "78f821a68e92f95b0cec7e9c46c07258",
+    "url": "/NewsApp/static/js/2.4847d574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c0b92a3de531aaa3a8",
+    "url": "/NewsApp/static/js/main.6b3a79ca.chunk.js"
+  },
+  {
+    "revision": "3b9b497e5f619cfce1dc",
+    "url": "/NewsApp/static/js/runtime-main.64ac8f5f.js"
+  },
+  {
+    "revision": "ef43c5c9cf5065c287422ae7b16e4ddd",
+    "url": "/NewsApp/static/media/beach.ef43c5c9.mp4"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/NewsApp/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/NewsApp/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/NewsApp/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/NewsApp/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/NewsApp/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "41bb112d036b2a0547605ea0f30b3674",
+    "url": "/NewsApp/static/media/cold.41bb112d.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/NewsApp/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/NewsApp/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/NewsApp/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/NewsApp/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/NewsApp/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/NewsApp/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/NewsApp/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/NewsApp/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/NewsApp/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/NewsApp/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/NewsApp/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "f7e0bd52807636458b7e86912dbe126e",
+    "url": "/NewsApp/static/media/warm.f7e0bd52.jpg"
+  },
+  {
+    "revision": "785672ff1b63d76beacf35793fe94f8a",
+    "url": "/NewsApp/static/media/weather1.785672ff.jpg"
+  }
+]);
