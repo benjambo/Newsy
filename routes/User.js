d568d4c89519b1be945ed1cc1dd62758bab916bc
Benjamin Bowo <ben.bowo@gmail.com>
2020-10-12 21:47:17 +0300

    Fixing

--- routes/User.js
+++ routes/User.js
@@ -3,7 +3,8 @@ const userRouter = express.Router()
 const passport = require('passport')
 const JWT = require('jsonwebtoken')
 const User = require('../models/User')
-
+const passportConfig = require('../passport')
+const News = require('../models/News')
 
 const signToken = (userID) => {
   return JWT.sign(
